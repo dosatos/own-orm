1b5315b05e7f5871a5b7cc46db1f6b7f7779f122
yb2n17@soton.ac.uk <yb2n17@soton.ac.uk>
2018-09-21 23:52:51 +0100

    create table, add isntance added

--- core/manager.py
+++ core/manager.py
@@ -9,3 +9,27 @@ __version__ = 'ORM Version 0.1'
 def create_engine(db_name):
     conn = sqlite3.connect(db_name)
     return conn
+
+
+class Session:
+    def __init__(self, bind=None):
+        self.engine = bind
+
+    def configure(self, bind=None):
+        self.engine = bind
+
+    def create_table(self, instance):
+        query = f"CREATE TABLE IF NOT EXISTS {instance.__tablename__} ({', '.join(instance.fields)})"
+        c = self.engine.cursor()
+        c.execute(query)
+
+    def add(self, instance):
+        c = self.engine.cursor()
+        qmark = '?'
+        params = f"{', '.join([qmark for _ in range(len(instance.fields))])}"
+        c.execute(f'INSERT INTO {instance.__tablename__} VALUES ({params})', instance.fields)
+
+
+    def save(self):
+        self.engine.commit()
+        self.engine.close()
--- tests/test_main.py
+++ tests/test_main.py
@@ -2,8 +2,7 @@ import os
 import glob
 import pytest
 
-from orm.core import manager
-from orm.core import models
+from orm.core import manager, models
 
 
 class User(models.Model):
@@ -52,13 +51,42 @@ class TestORM:
         db_name = 'sqlite.db'
         manager.create_engine(db_name)
         assert db_name in glob.glob('*')
-    #
-    # def test_create_table_in_db(self, engine):
-    #     pass
+
+    def test_session_bind_added(self, engine):
+        session = manager.Session(bind=engine)
+        assert session.engine == engine
+
+    def test_session_bind_added_through_configuration(self, engine):
+        session = manager.Session()
+        session.configure(bind=engine)
+        assert session.engine == engine
+
+    def test_table_created_through_session_instance(self, engine):
+        session = manager.Session(bind=engine)
+        user = User(name="Yeldos", email="yeldos@gmail.com")
+        session.create_table(user)
+        session.save()
+
+        engine = manager.create_engine('sqlite.db')
+        c = engine.cursor()
+        name = c.execute(f'SELECT name FROM {user.__tablename__}')
+        assert user.name.lower() in name.fetchone()
+
+    def test_instance_added_through_session(self, engine):
+        session = manager.Session(bind=engine)
+        user = User(name="Yeldos", email="yeldos@gmail.com")
+        session.add(user)
+        session.save()
+
+        engine = manager.create_engine('sqlite.db')
+        c = engine.cursor()
+        name = c.execute(f'SELECT name FROM {user.__tablename__}')
+        assert user.name.lower() in name.fetchone()
+
     #
     # def test_query_db(self):
     #     pass
-    #
+
     # def test_create_table_success(self, engine):
     #     session = manager.Session(bind=engine)
     #     session.add(User())
