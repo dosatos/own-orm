dd4ba43a7a5a394b05923df438dd0883808e7268
yb2n17@soton.ac.uk <yb2n17@soton.ac.uk>
2018-09-21 20:41:19 +0100

    test for db creation added (sqlite3 used)

--- core/manager.py
+++ core/manager.py
@@ -1,5 +1,12 @@
+import re
 import sqlite3
 
 
 
 __version__ = 'ORM Version 0.1'
+
+
+def create_engine(db_name):
+    conn = sqlite3.connect(db_name)
+    print(glob.glob('*'))
+    return conn
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,3 +1,5 @@
+import os
+import glob
 import pytest
 
 from orm.core import manager
@@ -6,4 +8,6 @@ from orm.core import manager
 class TestORM:
 
     def test_engine_created_success(self):
-        engine = create_engine()
+        db_name = 'sqlite.db'
+        manager.create_engine(db_name)
+        assert db_name in glob.glob('*')
